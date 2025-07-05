2227add6fdc3a1337e8ddffa27199e4989004fff
EvgenVelychko <evgenvelicko4@gmail.com>
2025-07-05 12:00:26 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sonara/',
+  base: '/sonora/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
